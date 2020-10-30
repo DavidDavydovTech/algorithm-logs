1502f7cdf538e360b48244a31de004dc1ab4ed46
David J. Davydov <davydovjd611@gmail.com>
2020-10-30 09:45:55 -0400

    Add islandCount problem statement

---
+++ algorithms/islandCount.js
@@ -0,0 +1,9 @@
+// The "Island Count" Problem
+
+// Given a two dimensional array of 1s and 0s, count the number of 
+// "islands" of 1s.
+
+// An "island" is a group of adjacent values that are all 1s. Every cell 
+// in the array can be adjacent to the two values that are next to it in 
+// the same dimension or the two that share it's index in a neighboring dimension.
+

9ac0088914c729a8b63d4e69eb59adea4275fd61
David J. Davydov <davydovjd611@gmail.com>
2020-11-09 11:21:51 -0500

    Add comments describing the problem

---
+++ algorithms/medianStream.js
@@ -0,0 +1,16 @@
+// The 'Median Stream' problem.
+
+// Given a stream of unordered integers, find the median of the stream. 
+// We will be asked to find the median multiple times, so the peekMedian
+// function should have optimal time complexity.
+
+// The "median" is the middle number of a sorted, odd-length set or
+// the average of the two middle numbers in a sorted, even-length set.
+
+// Examples:
+
+// [1, 4, 8]
+// Median is 4 (middle number)
+
+// [1, 3, 7, 8]
+// Median is 5 (average of two middle numbers)
